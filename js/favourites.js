8119a6ab7ec775cc059ff979f45fe57a659e69fc
lexahell <aleksey-grigorev7@mail.ru>
2023-11-28 21:08:57 +0300

    Добавить при пустом избранном текст

--- js/favourites.js
+++ js/favourites.js
@@ -9,6 +9,7 @@ arrJsons.push(bestseller,novelty, klassicheskayaProza, manga, comics, fantasy);
 const packageCategory= ["bestseller","novelty", "klassicheskayaProza", "manga", "comics", "fantasy"]
 const containerFavourites = document.querySelector(".container-favourites");
 for (let key in localStorage){
+    console.log(key);
     let isFound = false;
     let indexCategory = 0;
     for(let obj of arrJsons){
@@ -60,7 +61,7 @@ for (let key in localStorage){
                 const id = newProductCardSlide.dataset.chgProductId;
                 const likeButton = newProductCardSlide.querySelector(".favourite-button");
     
-                // Определям находится ди товар в избранном и меняем кнопку лайка
+                // Определям находится ли товар в избранном и меняем кнопку лайка
                 if (localStorage.getItem(`book_${id}`)) {
                     likeButton.classList.add("favourite-button-active");
                     newProductCardSlide.dataset.chgFavouritesStatus = "true";
@@ -117,5 +118,28 @@ for (let key in localStorage){
     indexCategory = 0;
 }
 if (containerFavourites.querySelector("*") == null){
-    containerFavourites.textContent = "В избранном пока пусто";
+    containerFavourites.remove();
+    const emptyFavourites = `
+    <div class="container-empty">
+            <h3 class="title-empty-favourites">Здесь пока пусто</h3>
+            <br>
+            <p class="empty-text">
+              <span>
+                Чтобы добавлять товары, нажимайте на 
+              </span>
+              <svg  height="16px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <g data-name="Layer 54" id="Layer_54">
+                    <path
+                    class="cls-1"
+                    d="M16,28.72a3,3,0,0,1-2.13-.88L3.57,17.54a8.72,8.72,0,0,1-2.52-6.25,8.06,8.06,0,0,1,8.14-8A8.06,8.06,0,0,1,15,5.68l1,1,.82-.82h0a8.39,8.39,0,0,1,11-.89,8.25,8.25,0,0,1,.81,12.36L18.13,27.84A3,3,0,0,1,16,28.72ZM9.15,5.28A6.12,6.12,0,0,0,4.89,7a6,6,0,0,0-1.84,4.33A6.72,6.72,0,0,0,5,16.13l10.3,10.3a1,1,0,0,0,1.42,0L27.23,15.91A6.25,6.25,0,0,0,29,11.11a6.18,6.18,0,0,0-2.43-4.55,6.37,6.37,0,0,0-8.37.71L16.71,8.8a1,1,0,0,1-1.42,0l-1.7-1.7a6.28,6.28,0,0,0-4.4-1.82Z"
+                />
+                </g>
+                </svg>
+            </p>
+          </div>
+    `
+    const favWrapper =  document.querySelector(".fav-wrapper");
+    favWrapper.insertAdjacentHTML("beforeend", emptyFavourites);
+    
+    // containerFavourites.textContent = "В избранном пока пусто";
 }
